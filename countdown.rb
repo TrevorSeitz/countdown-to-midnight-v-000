fe7bdcf4036a715d0337ba37c341eb804408abd1
Trevor Seitz <trevinla@gmail.com>
2018-03-12 16:45:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,10 @@
 def countdown(n)
   counter = n
   while counter > 0
-    puts "#{counter} SECOND(S)!"
+    "#{counter} SECOND(S)!"
     counter -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(n)
